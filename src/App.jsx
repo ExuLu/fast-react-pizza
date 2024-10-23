51443efa5388c787d6295e8e8177d6d187e28ae1
Alena Sharai <exultantislupus@gmail.com>
2024-10-23 16:56:24 +0400

    feat: create route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './UI/Home.jsx';
+import Menu from './features/menu/Menu.jsx';
+import Cart from './features/cart/Cart.jsx';
+import CreateOrder from './features/order/CreateOrder.jsx';
+import Order from './features/order/Order.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/menu',
+    element: <Menu />,
+  },
+  {
+    path: '/cart',
+    element: <Cart />,
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />,
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />,
+  },
+]);
+
 const App = () => {
-  return <div>Hello Vite!</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
