ea83adbfb8ab2d2a3ecfa984e57ded985237ed29
Alena Sharai <exultantislupus@gmail.com>
2024-10-23 17:03:17 +0400

    feat: add main and cart overview to layout, change link in cart

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -1,9 +1,14 @@
 import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
 
 const AppLayout = () => {
   return (
     <div>
       <Header />
+      <main>
+        <h1>Content</h1>
+      </main>
+      <CartOverview />
     </div>
   );
 };
