899e07eaf838aedb66d4c6e4d6622f759749db9d
Alena Sharai <exultantislupus@gmail.com>
2024-10-24 18:35:45 +0400

    feat: create handleSubmit

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchOrder = () => {
   const [query, setQuery] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery('');
   };
 
   return (
