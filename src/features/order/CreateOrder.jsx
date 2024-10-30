aa004fddff70a88acbea0c267181f2df510ad0f8
Alena Sharai <exultantislupus@gmail.com>
2024-10-30 17:48:36 +0400

    feat: style links and buttons

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -71,7 +71,10 @@ const CreateOrder = () => {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button disabled={isLoading}>
+          <button
+            className='inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-600'
+            disabled={isLoading}
+          >
             {isLoading ? 'Placing order...' : 'Order now'}
           </button>
         </div>
