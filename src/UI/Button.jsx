f218bbe4a8707ece67518fd81b57b52c99f850c4
Alena Sharai <exultantislupus@gmail.com>
2024-10-31 15:17:41 +0400

    feat: add to option to button

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,17 +1,28 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-const Button = ({ children, isDisabled }) => (
-  <button
-    className='inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-600 sm:px-6 sm:py-4'
-    disabled={isDisabled}
-  >
-    {children}
-  </button>
-);
+const Button = ({ children, isDisabled, to }) => {
+  const className =
+    'inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-600 sm:px-6 sm:py-4';
+
+  if (to)
+    return (
+      <Link className={className} to={to}>
+        {children}
+      </Link>
+    );
+
+  return (
+    <button className={className} disabled={isDisabled}>
+      {children}
+    </button>
+  );
+};
 
 Button.propTypes = {
   children: PropTypes.string || PropTypes.element,
   isDisabled: PropTypes.bool,
+  to: PropTypes.string,
 };
 
 export default Button;
