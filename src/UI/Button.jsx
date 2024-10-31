001d58b346c15278c009700e6a996d7ea3f9ceed
Alena Sharai <exultantislupus@gmail.com>
2024-10-31 14:45:24 +0400

    refactor: create Button component

---
+++ src/UI/Button.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const Button = ({ children, isDisabled }) => (
+  <button
+    className='inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-600'
+    disabled={isDisabled}
+  >
+    {children}
+  </button>
+);
+
+Button.propTypes = {
+  children: PropTypes.string || PropTypes.element,
+  isDisabled: PropTypes.bool,
+};
+
+export default Button;
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Form, useActionData, useNavigation } from 'react-router-dom';
+import Button from '../../UI/Button';
 
 const fakeCart = [
   {
@@ -72,12 +73,9 @@ const CreateOrder = () => {
 
         <div>
           <input type='hidden' name='cart' value={JSON.stringify(cart)} />
-          <button
-            className='inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-yellow-600'
-            disabled={isLoading}
-          >
+          <Button isDisabled={isLoading}>
             {isLoading ? 'Placing order...' : 'Order now'}
-          </button>
+          </Button>
         </div>
       </Form>
     </div>
