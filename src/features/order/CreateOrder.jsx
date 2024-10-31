3f256c11d322baf75accd76e4583a0293845cb79
Alena Sharai <exultantislupus@gmail.com>
2024-10-31 18:50:17 +0400

    feat: add styles to input fields

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -35,27 +35,32 @@ const CreateOrder = () => {
   const cart = fakeCart;
 
   return (
-    <div className='my-10'>
-      <h2>Ready to order? Let's go!</h2>
+    <div className='px-4 py-6'>
+      <h2 className='mb-8 text-xl font-semibold'>Ready to order? Let's go!</h2>
 
       <Form method='POST'>
-        <div>
-          <label>First Name</label>
-          <input className='input' type='text' name='customer' required />
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>First Name</label>
+          <input className='input grow' type='text' name='customer' required />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className='input' type='tel' name='phone' required />
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>Phone number</label>
+          <div className='grow'>
+            <input className='input w-full' type='tel' name='phone' required />
+            {formErrors?.phone && <p>{formErrors.phone}</p>}
           </div>
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input className='input' type='text' name='address' required />
+        <div className='mb-5 flex flex-col gap-2 sm:flex-row sm:items-center'>
+          <label className='sm:basis-40'>Address</label>
+          <div className='grow'>
+            <input
+              className='input w-full'
+              type='text'
+              name='address'
+              required
+            />
           </div>
         </div>
 
